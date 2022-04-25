2ed9682443e2ae2b4e1f9923f50fec4187f4ce49
hilal.somunkiran <hilal.somunkiran@hmb.gov.tr>
2022-04-25 11:48:14 +0300

    Kelimeler Türkçe olarak değiştirildi. Klavye türkçe karakterlere göre düzenlendi.

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -38,9 +38,11 @@ export const Keyboard = ({
       } else if (e.code === 'Backspace') {
         onDelete()
       } else {
+        //  console.log(e);
         const key = localeAwareUpperCase(e.key)
+         // console.log(key);
         // TODO: check this test if the range works with non-english letters
-        if (key.length === 1 && key >= 'A' && key <= 'Z') {
+        if (key.length === 1 && key >= 'A' && key <= 'Ş' && key!=='Q' && key!=='W' && key!=='X') {
           onChar(key)
         }
       }
@@ -54,7 +56,7 @@ export const Keyboard = ({
   return (
     <div>
       <div className="flex justify-center mb-1">
-        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
+        {['E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Ğ', 'Ü'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -65,7 +67,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center mb-1">
-        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map((key) => (
+        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ş', 'İ'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -79,7 +81,7 @@ export const Keyboard = ({
         <Key width={65.4} value="ENTER" onClick={onClick}>
           {ENTER_TEXT}
         </Key>
-        {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
+        {['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Ö', 'Ç'].map((key) => (
           <Key
             value={key}
             key={key}
